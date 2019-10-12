21d16426c551bcfaa0b4f247ca5c948e9e9f183a
Hatoon Mohammed <ahotoon@gmail.com>
2019-10-12 17:08:49 +0300

    final touches

--- src/Cart.js
+++ src/Cart.js
@@ -31,6 +31,13 @@ class Cart extends Component {
     <span class="input-group-text" id="inputGroup-sizing-default">Address</span>
   </div>
   <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">Phone#</span>
+  </div>
+  <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
+
+
 </div>
 </tr>
         <tr>
